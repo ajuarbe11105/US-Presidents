2989d3064b96adfa21508e52a5a5f92f1a3be9cd
alberto Juarbe <albertojuarbe2@gmail.com>
2022-02-05 22:21:18 -0500

    Initial Commit

---
+++ USPresidents/USPresidentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  USPresidentsApp.swift
+//  USPresidents
+//
+//  Created by Alberto Juarbe on 2/5/22.
+//
+
+import SwiftUI
+
+@main
+struct USPresidentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
